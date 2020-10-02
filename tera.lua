e92d1ef22d7118c9b007d69b85e083038c92013c
InaccurateTank <51210312+InaccurateTank@users.noreply.github.com>
2020-10-02 04:42:12 -0700

    absolute clusterfuck error handling attempt

--- tera.lua
+++ tera.lua
@@ -13,17 +13,59 @@ local ver = 1.0
 local progName = "/tank/tera"
 
 local reactor = component.reactor_redstone_port
-local inv = component.inventory_controller
-local tank = component.tank_controller
-local rs = component.redstone
-local cap = nil
+local inv = component.proxy("001188a5-1236-436e-9838-a54943cbedc1")
+local tank = component.proxy("2761ed08-1aa9-49d7-bad2-581f23794cf3")
+local rs = component.proxy("a5de26a1-7e24-461d-8a15-d02ced226ece")
+-- local cap = component.proxy()
 local cSide = sides.east -- Reactor Tank/Inv Side
 local rSide = sides.down -- Reactor Redstone Side
 
 local rods = {}
 local overide = false
 local prog = GUI.manager()
-prog.back = 0xcccccc
+prog.back = 0x000000--0xcccccc
+
+-----Vital Functions-----
+local function close(e)
+  rs.setOutput(rSide, 0)
+  GUI.invertTouch(false)
+  prog:stop()
+  if e == 1 then
+    error("Reactor does not exist.  Please check the block formation for explosions.")
+  end
+  os.exit()
+end
+
+local function checkReactor()
+  if not component.get(component.reactor_redstone_port.address) then -- See if the reactor even exists
+    local rbuffer = rs.setOutput(rSide, 0) -- if not, shut off reactor
+    -- TODO: some code to do UI stuff when this is happening
+    for i = 1, 5 do -- Check if reactor exists again once per second for 5 seconds
+      if component.get(component.reactor_redstone_port.address) then
+        print("?")
+        if rbuffer > 0 then -- Restart reactor if it was on
+          rs.setOutput(rSide, 15)
+        end
+        -- TODO: UI stuff
+        return true
+      elseif i == 5 then -- Has those 5 seconds elapsed
+        return false
+      else
+        os.sleep(1)
+      end
+    end
+  else
+    return true
+  end
+end
+
+local function errChk(comp, fun)
+  if checkReactor(comp) then
+    return fun
+  else
+    close(1)
+  end
+end
 
 -----Main Window-----
 -- local exit = GUI.newButton(prog, 80, 1, 1, 1, 0xff3333, 0xff3333, 0xffffff, 0xffffff, " ")
@@ -35,33 +77,11 @@ prog.back = 0xcccccc
 -----Energy Window-----
 
 -----Program Functions-----
-local function checkReactor()
-  if not component.get(reactor.address) then -- See if the reactor even exists
-    local rbuffer = rs.setOutput(rSide, 0) -- if not, shut off reactor
-    -- TODO: some code to do UI stuff when this is happening
-    for i = 1, 5 do -- Check if reactor exists again once per second for 5 seconds
-      if component.get(reactor.address) ~= nil then
-        if rbuffer > 0 then -- Restart reactor if it was on
-          rs.setOutput(rSide, 15)
-        end
-        -- TODO: UI stuff
-        return true
-      elseif i == 5 then -- Has those 5 seconds elapsed
-        return false
-      else
-        i = i + 1
-        os.sleep(1)
-      end
-    end
-  else
-    return true
-  end
-end
-
-local function checkFuel(fuel, deep)
-  if deep then
+local function checkFuel(fuel, deep) -- Checks reactor fuel rods
+  if deep then -- Deep scan checks the entire reactor inventory.  Use sparingly.
     fuel = {}
     for i = 1, 54 do
+      print(i)
       local slot = inv.getStackInSlot(cSide, i)
       if slot ~= nil then
         if string.find(slot.label, "Fuel") then
@@ -69,9 +89,10 @@ local function checkFuel(fuel, deep)
         end
       end
     end
-  else
+  else -- Quick scan checks only the slots that had rods in them last.
     for k, v in pairs(fuel) do
       fuel[k] = inv.getStackInSlot(cSide, k)
+      print(k)
     end
   end
   return fuel
@@ -79,8 +100,10 @@ end
 
 local function reactorControl(status)
   if status.active == 0 then
+    print("offline")
     -- TODO: Actual UI stuff, Reactor Offline
   else
+    print("online")
     -- TODO: Actual UI stuff, Reactor Online
   end
   --print(status[3])
@@ -94,9 +117,8 @@ local function reactorControl(status)
     elseif status.heatPercent >= 70 then -- 70% however is just radiation leaks.
         print("radwarning") -- TODO: Actual UI stuff, Radiation
     end
-    rods = checkFuel(rods)
     for k, v in pairs(rods) do
-      if rods[k].damage == 0 then
+      if rods[k].damage == 0 or rods == {} then
         print("fuel depleted") -- TODO: Actual UI stuff, Fuel Depleted
         -- TODO: set activator to lock until deep fuel scan
         rs.setOutput(rSide, 0)
@@ -110,12 +132,6 @@ local function capControl()
 end
 
 -----Common Commands-----
-local function close()
-  rs.setOutput(rSide, 0)
-  GUI.invertTouch(false)
-  prog:stop()
-  os.exit()
-end
 
 -----Button Init-----
 
@@ -130,24 +146,24 @@ rods = checkFuel(rods, true)
 
 repeat
   if checkReactor() then
-    local status = {active = reactor.getReactorEUOutput(),
-      heat = reactor.getHeat(),
-      heatPercent = math.floor((math.min(reactor.getHeat(), reactor.getMaxHeat()) / reactor.getMaxHeat()) * 100),
-      coolent = tank.getFluidInTank(cSide),
-      coolTank = tank.getFluidInTank(cSide)[1].amount,
-      coolPercent = math.floor((math.min(tank.getFluidInTank(cSide)[1].amount, 10000) / 10000) * 100),
-      hotTank = tank.getFluidInTank(cSide)[2].amount,
-      hotPercent = math.floor((math.min(tank.getFluidInTank(cSide)[2].amount, 10000) / 10000) * 100)
+    local a = reactor.getReactorEUOutput()
+    local h = reactor.getHeat()
+    local mH = reactor.getMaxHeat()
+    local t = tank.getFluidInTank(cSide)
+    local status = {
+      active = a,
+      heat = h,
+      heatPercent = math.floor((math.min(h, mH) / mH) * 100),
+      coolent = t,
+      coolTank = t[1].amount,
+      coolPercent = math.floor((math.min(t[1].amount, 10000) / 10000) * 100),
+      hotTank = t[2].amount,
+      hotPercent = math.floor((math.min(t[2].amount, 10000) / 10000) * 100)
     }
+    print(status.active)
     rods = checkFuel(rods)
     reactorControl(status)
     capControl()
     os.sleep(0.25)
-  else
-    rs.setOutput(rSide, 0)
-    GUI.invertTouch(false)
-    prog:stop()
-    error("Reactor does not exist.  Please check the block formation for explosions.")
-    os.exit()
   end
 until not prog.run
