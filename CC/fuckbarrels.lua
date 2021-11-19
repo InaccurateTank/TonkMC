104c4565220e21f3cf66f774026a3f9a4d195a31
InaccurateTank <51210312+InaccurateTank@users.noreply.github.com>
2021-11-19 05:32:51 -0800

    Optimized

--- CC/fuckbarrels.lua
+++ CC/fuckbarrels.lua
@@ -3,22 +3,15 @@
 
 while true do
   turtle.dig()
-  for i=1,16 do
+  for i=1,8 do
     turtle.select(i)
-    local detail = turtle.getItemDetail(i)
-    if detail and detail.name == "minecraft:barrel" then
-      detail = turtle.getItemDetail(i, true)
-      if detail.lore and detail.lore[1] == "The fabled prize awaits at the bottom..." then
+    local d = turtle.getItemDetail(i,true)
+    if d then
+      if d.lore and d.lore[1] == "The fabled prize awaits at the bottom..." then
         turtle.place()
-        break
+      else
+        turtle.dropUp()
       end
-    end
-  end
-  for i=1,16 do
-    turtle.select(i)
-    local detail = turtle.getItemDetail(i)
-    if detail then
-      turtle.dropUp()
     end
   end
   turtle.select(1)
